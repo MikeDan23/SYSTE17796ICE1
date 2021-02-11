4810daf21b4c667230d57175a797ee2ce2e69067
Michael Danso <dansomi@sheridancollege.ca>
2021-02-11 17:43:15 -0500

    First Commit

---
+++ src/assignment/pkg1/in/pkgclass/excersize/Assignment1InClassExcersize.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment.pkg1.in.pkgclass.excersize;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author mic_d
+ */
+public class Assignment1InClassExcersize {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Scanner temp = new Scanner(System.in);
+
+        String arr[] = {"Day 1 AM", "Day 1 PM", "Day 2 AM", "Day 2 PM", "Day 3 AM", "Day 3 PM", "Day 4 AM", "Day 4 PM", "Day 5 AM", "Day 5 PM", "Day 6 AM", "Day 6 PM", "Day 7 AM", "Day 7 PM"};
+        
+        int sum = 0;
+        int tempsCount = 0;
+        int CalcAvg = 0;
+
+        while (tempsCount < 14) {
+            System.out.println("Enter the temperature for " + arr[tempsCount] + ": ");
+            CalcAvg = temp.nextInt();
+            if (CalcAvg > 50 || CalcAvg < -50) {
+                throw new IllegalArgumentException("Temperatures cannot be less than -50 or greater than 50");
+            }
+            tempsCount++;
+            sum = sum + CalcAvg;
+        }
+        
+        CalcAvg = sum / 14;
+        System.out.println("the avg tempaetaure is " + CalcAvg + "°");
+    }
+}
